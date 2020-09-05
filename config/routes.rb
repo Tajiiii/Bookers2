8ab2aeaf2e63175f4c57a273ea989e183b981886
Tajiiii <daisuke.tajima.0218@gmail.com>
2020-09-05 22:11:18 +0900

    add search

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ Rails.application.routes.draw do
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  get 'search' => 'searches#search'
 
 
 end
